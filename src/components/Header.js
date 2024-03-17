8255192850f2c4a44d219f3db5d9c2d5da2b58df
mountsamson <162016378+mountsamson@users.noreply.github.com>
2024-03-17 17:23:37 +1100

    Add files via upload

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <div>
+        <h2 id="TRW">Expense Tracker - The Real World Bank</h2>
+        <img id="bigG" src="https://www.jointherealworld.com/revamp/images/the-real-world-logo_1the-real-world-logo.webp" />
+
+    </div>
+  )
+}
